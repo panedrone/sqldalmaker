84ca34904b602e1e2d399f33c5a5a413cdd8cb6a
panedrone <panedrone@PANEDRONE-PC>
2019-08-17 13:03:14 +0300

    Fixes of DTO navigation in Eclipse

--- src_eclipse/com/sqldalmaker/eclipse/EclipseXmlUtils.java
+++ src_eclipse/com/sqldalmaker/eclipse/EclipseXmlUtils.java
@@ -42,18 +42,29 @@ public class EclipseXmlUtils {
 
 			} else {
 
-				int offset = location.getCharacterOffset();
+				// https://stackoverflow.com/questions/3176610/java-gathering-byte-offsets-of-xml-tags-using-an-xmlstreamreader
+				// You could use getLocation() on the XMLStreamReader (or XMLEvent.getLocation()
+				// if you use XMLEventReader),
+				// but I remember reading somewhere that it is not reliable and precise.
+				// And it looks like it gives the endpoint of the tag, not the starting
+				// location.
+				
+				// int offset = location.getCharacterOffset();
 
+				int line = location.getLineNumber();
+				
+				int offset = styledText.getOffsetAtLine(line - 1); // line index needed
+				
 				styledText.setCaretOffset(offset);
 
 				// https://stackoverflow.com/questions/6321517/getting-swt-styledtext-widget-to-always-scroll-to-its-end
 
-				int line = location.getLineNumber() - 3;
-				
+				line = line - 3;
+
 				if (line < 0) {
 					line = 0;
 				}
-				
+
 				styledText.setTopIndex(line);
 			}
 
@@ -62,6 +73,30 @@ public class EclipseXmlUtils {
 			throw new Exception("Cannot read " + Const.DTO_XML);
 		}
 
+	}
+
+	private static boolean is_dto_class_element(XMLStreamReader streamReader, String dto_class_name) {
+
+		int attr_count = streamReader.getAttributeCount(); // it works only with event == START_ELEMENT
+
+		for (int i = 0; i < attr_count; i++) {
+
+			String attr_name = streamReader.getAttributeLocalName(i); // getAttributeName(i).getLocalPart();
+
+			if (attr_name.equals("name")) {
+
+				String attr_value = streamReader.getAttributeValue(i);
+
+				if (attr_value.equals(dto_class_name)) {
+
+					return true;
+				}
+
+				return false;
+			}
+		}
+
+		return false;
 	}
 
 	private static Location get_offset(String xml_text, String dto_class_name) throws Exception {
@@ -74,37 +109,34 @@ public class EclipseXmlUtils {
 
 			streamReader.next();
 
+			// https://stackoverflow.com/questions/3176610/java-gathering-byte-offsets-of-xml-tags-using-an-xmlstreamreader
+			// You could use getLocation() on the XMLStreamReader (or XMLEvent.getLocation()
+			// if you use XMLEventReader),
+			// but I remember reading somewhere that it is not reliable and precise.
+			// And it looks like it gives the endpoint of the tag, not the starting
+			// location.
+
 			if (streamReader.getEventType() == XMLStreamReader.START_ELEMENT) {
 
 				String tag = streamReader.getLocalName();
 
 				if (tag.compareTo("dto-class") == 0) {
 
-					int attr_count = streamReader.getAttributeCount();
+					if (is_dto_class_element(streamReader, dto_class_name)) {
 
-					for (int i = 0; i < attr_count; i++) {
-
-						String attr_name = streamReader.getAttributeName(i).getLocalPart();
-
-						if (attr_name.equals("name")) {
-
-							String attr_value = streamReader.getAttributeValue(i);
-
-							if (attr_value.equals(dto_class_name)) {
-
+//						while (streamReader.hasNext()) {
+//
+//							streamReader.next();
+//
+//							if (streamReader.getEventType() == XMLStreamReader.END_ELEMENT) {
+							
 								Location location = streamReader.getLocation();
 
-								// return location.getCharacterOffset();
-
-//								char[] cc = streamReader.getTextCharacters();
-//								
-//								String et = streamReader.getText(); 
-//								
-//								return location.getCharacterOffset() - et.length();
-
 								return location;
-							}
-						}
+//							}
+//						}
+//						
+//						return null;
 					}
 				}
 			}
