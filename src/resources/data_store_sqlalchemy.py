266382c6a0373bf1be0d9153b983eaddcf80ea38
panedrone <sqldalmaker@gmail.com>
2022-04-23 08:30:12 +0300

    improvements of data_store_sqlalchemy.py

--- src/resources/data_store_sqlalchemy.py
+++ src/resources/data_store_sqlalchemy.py
@@ -75,7 +75,11 @@ class DataStore:
             self.conn.close()
             self.conn = None
 
-    def start_transaction(self):
+    def begin(self):
+        if self.transaction is None:
+            # https://docs.sqlalchemy.org/en/14/orm/session_transaction.html
+            self.session.begin()
+            return
         # https://docs.sqlalchemy.org/en/14/core/connections.html
         self.transaction = self.conn.begin()
 
@@ -107,13 +111,25 @@ class DataStore:
         # connection = self.connect(close_with_result=True) ---- no need because of connected
         if params is None:
             params = []
+
         # with self._exec(cls.SQL, params) as rows: # .fetchall() -- AttributeError: __enter__
         cursor = self._exec(cls.SQL, params)
         try:
+            # https://stackoverflow.com/questions/31750441/generalised-insert-into-sqlalchemy-using-dictionary
+            # https://stackoverflow.com/questions/3451779/how-to-dynamically-create-an-instance-of-a-class-in-python
             # https://stackoverflow.com/questions/1958219/how-to-convert-sqlalchemy-row-object-to-a-python-dict
-            return [dict(row) for row in cursor]
+            res = [cls(**dict(row)) for row in cursor]
+            return res
         finally:
             cursor.close()
+
+    def get_one(self, cls, params=None):
+        rows = self.get_all(cls, params)
+        if len(rows) == 0:
+            raise Exception('No rows')
+        if len(rows) > 1:
+            raise Exception('More than 1 row exists')
+        return rows[0]
 
     def insert_row(self, sql, params, ai_values):
         """
