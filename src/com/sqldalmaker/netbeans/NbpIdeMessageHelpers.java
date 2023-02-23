6de520ab25e456ff636c4126bd1bb4ab5e0d517d
Andrii M <andrey.meretsky@jetsoftpro.com>
2023-02-23 15:42:16 +0200

    1.279

---
+++ src/com/sqldalmaker/netbeans/NbpIdeMessageHelpers.java
@@ -0,0 +1,56 @@
+/*
+    Copyright 2011-2021 sqldalmaker@gmail.com
+    SQL DAL Maker Website: http://sqldalmaker.sourceforge.net
+    Read LICENSE.txt in the root of this project/archive for details.
+ */
+package com.sqldalmaker.netbeans;
+
+import com.sqldalmaker.common.InternalException;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author sqldalmaker@gmail.com
+ *
+ */
+public class NbpIdeMessageHelpers {
+
+    public static void show_error_in_ui_thread(final Throwable e) {
+        // https://platform.netbeans.org/tutorials/nbm-hyperlink.html
+        // RequestProcessor.getDefault().post(
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                String msg = "";
+                if (e != null) {
+                    if (!(e instanceof InternalException)) {
+                        msg += e.getClass().getName() + ":\n";
+                    }
+                    msg += e.getMessage();
+                } else {
+                    msg = "Unknown Error";
+                }
+                JOptionPane.showMessageDialog(null, msg, "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+    }
+
+    public static void show_error_in_ui_thread(final String msg) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JOptionPane.showMessageDialog(null, msg, "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+    }
+
+    public static void show_info_in_ui_thread(final String msg) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JOptionPane.showMessageDialog(null, msg, "Info", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+    }
+}
